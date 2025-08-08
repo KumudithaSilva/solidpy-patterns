000a2c6c011c2fb70c813869516edc8a368bb0d9
Kumuditha <kumudithasilva66@gmail.com>
2025-08-08 18:31:27 +0530

    Interface implemented

--- Basic_OOP/Interface.py
+++ Basic_OOP/Interface.py
@@ -13,9 +13,24 @@ class SecondClass(MyInterface):
     def my_method(self):
         print("This method implemented in SecondClass")
 
+class ThirdClass:
+    def some_method(self):
+        print("This class not implementing the MyInterface")
+
+def process_my_interface(obj: MyInterface):
+    obj.my_method()
+    print("This object implemented MyInterface")
+
 
 my_first = FirstClass()
-my_first.my_method()
+# my_first.my_method()
 
 my_second = SecondClass()
-my_second.my_method()
+# my_second.my_method()
+
+my_third = ThirdClass()
+my_third.some_method()
+
+process_my_interface(my_first)
+process_my_interface(my_second)
+
