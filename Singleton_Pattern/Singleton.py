dde13b39f3d4df7aff890dec989ac88e20d1fe67
Kumuditha <kumudithasilva66@gmail.com>
2025-08-27 14:20:55 +0530

    singleton with class level

---
+++ Singleton_Pattern/Singleton.py
@@ -0,0 +1,13 @@
+class Singleton:
+    _instance = None
+
+    # override the new method which is control how object are created
+    def __new__(cls):
+
+        if not cls._instance:
+            # create a new singleton instance using the parent class's __new__
+            cls._instance = super().__new__(cls)
+        # return the singleton instance class or newly created one
+        return cls._instance
+
+s1 = Singleton()
