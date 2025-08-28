f21503a18c0a5578a3f26fcfb92425fe8e9f5021
Kumuditha <kumudithasilva66@gmail.com>
2025-08-28 20:28:00 +0530

    singleton with meta

---
+++ Singleton_Pattern/Singleton_Meta.py
@@ -0,0 +1,10 @@
+"""
+Meta Class - It is a class that define the behaviour and rules for creating other class.
+
+Metaclass allows to customize the class creation process and modify
+class attribute, methods, or the properties before the class actually created.
+
+By default, all Python classes implicitly inherit from the type build-class,
+which itself a Meta class.
+
+"""
