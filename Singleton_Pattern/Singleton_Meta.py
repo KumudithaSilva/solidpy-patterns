3922a4230c561b038d57d8b162a48428c4458507
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-29 18:11:06 +0530

    Add Singleton Meta Clas

--- Singleton_Pattern/Singleton_Meta.py
+++ Singleton_Pattern/Singleton_Meta.py
@@ -8,3 +8,23 @@ By default, all Python classes implicitly inherit from the type build-class,
 which itself a Meta class.
 
 """
+
+class SingletonMeta(type):
+    # Dictionary stores single instance of the class
+    # Each subclass of the SingletonMeta metaclass
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        # If single instance of the class already been created?
+        if cls not in cls._instance:
+            # If not, create the instance of the class that calling this
+            # that is Singleton class
+            instance = super().__call__()
+            cls._instance[cls] = instance
+        return cls._instance[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    def some_implementation(self):
+        pass
+
+s1 = Singleton()
