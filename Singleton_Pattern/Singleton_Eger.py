5adf997a1c8dc4d161112900adc55ab8be8c99aa
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-08-29 18:33:51 +0530

    Add Singleton Meta Clas with Eager Loading

---
+++ Singleton_Pattern/Singleton_Eger.py
@@ -0,0 +1,8 @@
+"""
+Eager Loading - The Idea of eager loading is load the instance before need it.
+
+It is useful for data preloading and caching, connectivity pre-caching and
+frequent and fast access.
+
+Python does not support eager instantiation, but using a metaclass, it can be achieved.
+"""
