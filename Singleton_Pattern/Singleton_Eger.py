781ebd0d9638b4d0c2ea56bc734abfe46c1468e0
Kumuditha <kumudithasilva66@gmail.com>
2025-09-01 18:42:46 +0530

    eager loading

--- Singleton_Pattern/Singleton_Eger.py
+++ Singleton_Pattern/Singleton_Eger.py
@@ -5,4 +5,19 @@ It is useful for data preloading and caching, connectivity pre-caching and
 frequent and fast access.
 
 Python does not support eager instantiation, but using a metaclass, it can be achieved.
+
+Eager loading can be achieved by called init method before call method
 """
+class SingletonMeta(type):
+    _instance = {}
+
+    def __init__(cls, name, bases, dct):
+        super().__init__(name, bases, dct)
+        cls._instance[cls] = super().__call__()
+
+    def __call__(cls, *args, **kwargs):
+        return cls._instance[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    def __init__(self):
+        pass
