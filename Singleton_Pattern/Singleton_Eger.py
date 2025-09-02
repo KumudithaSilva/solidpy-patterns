a51aa56b8bfcbe149f6df4d306d08d1ea8f8d979
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-02 18:32:25 +0530

    Add Singleton Meta Clas with Eager Loading and comments

--- Singleton_Pattern/Singleton_Eger.py
+++ Singleton_Pattern/Singleton_Eger.py
@@ -9,15 +9,28 @@ Python does not support eager instantiation, but using a metaclass, it can be ac
 Eager loading can be achieved by called init method before call method
 """
 class SingletonMeta(type):
+    # Dictionary stores single instance of the class
     _instance = {}
 
+    # This method is called when a class is created (not an object)
+    # override the call during creation of the subtypes
     def __init__(cls, name, bases, dct):
         super().__init__(name, bases, dct)
+
+        # Create and store a single instance of the class right away
+        # eager loading the class instance
         cls._instance[cls] = super().__call__()
 
+    # This method is called when trying to create (call) an object of the class
     def __call__(cls, *args, **kwargs):
+        # Instead of creating a new object, always return the same stored instance
         return cls._instance[cls]
 
 class Singleton(metaclass=SingletonMeta):
     def __init__(self):
-        pass
+        print("Initializing Singleton instance")
+
+obj1 = Singleton()
+obj2 = Singleton()
+
+print(obj1 is obj2)
