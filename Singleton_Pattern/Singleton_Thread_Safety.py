939aeee8cef373c525693299b897d45a98e1b745
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-04 18:55:52 +0530

    Thread Safety in Singleton

---
+++ Singleton_Pattern/Singleton_Thread_Safety.py
@@ -0,0 +1,27 @@
+"""
+Thread Safety in Singleton
+
+In a multithreaded environment, if multiple threads try to create a Singleton instance at the same time,
+it can lead to race conditions, resulting in multiple instances being created.
+"""
+import threading
+
+class ThreadingSafeSingleton:
+    # Class-level variable to store the single instance of the class
+    _instance = None
+    # Class-level lock to ensure thread safety during instance creation
+    _lock = threading.Lock()
+
+    # Override the __new__ method to control instance creation
+    def __new__(cls, *args, **kwargs):
+        # Acquire the lock to enter the critical section
+        with cls._lock:
+            # Check if the singleton instance has already been created
+            if cls._instance is None:
+                # Create the single instance of the class
+                cls._instance = super().__new__(cls)
+        # Return the single instance (existing or newly created)
+        return cls._instance
+
+# Create an instance of the singleton class
+s1 = ThreadingSafeSingleton()
