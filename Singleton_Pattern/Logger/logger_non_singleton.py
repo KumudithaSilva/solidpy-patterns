ca727af8d4c4ee64bb363c378083c686135a45ad
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-12 23:21:06 +0530

    Add logger

---
+++ Singleton_Pattern/Logger/logger_non_singleton.py
@@ -0,0 +1,29 @@
+import logging
+
+# Set up the logger
+logger = logging.getLogger("non_singleton_logger")
+logger.setLevel(logging.DEBUG)
+
+# Create file handler and set its level to DEBUG
+file_handler = logging.FileHandler("basic_file.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Create a console handler and set its level to INFO
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Create formater and add it to both handlers
+formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formater)
+console_handler.setFormatter(formater)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# Add loggers
+logger.debug("DEBUTING STARTED")
+logger.info("INFORMATION RECEIVED")
+logger.warning("WARNING RECEIVED")
+logger.error("ERROR OCCURRED")
+logger.critical("CRITICAL ALARM")
