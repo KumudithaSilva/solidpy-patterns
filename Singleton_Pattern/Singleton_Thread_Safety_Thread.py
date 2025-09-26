6f681e3deb209ab325d49421e41593e863b87842
Kumuditha Silva <kumudithasilva66@gmail.com>
2025-09-27 03:51:47 +0530

    add singleton number generator thread safe

---
+++ Singleton_Pattern/Singleton_Thread_Safety_Thread.py
@@ -0,0 +1,42 @@
+import threading
+
+class Number_Generator:
+    _instance = None
+    _init_number = 0
+    _lock = threading.Lock()
+
+    # override the new method which is control how object are created
+    def __new__(cls):
+
+        with cls._lock:
+            if not cls._instance:
+                # create a new singleton instance using the parent class's __new__
+                cls._instance = super(Number_Generator, cls).__new__(cls)
+                # return the singleton instance class or newly created one
+        return cls._instance
+
+    def generate_next_number(self):
+        with self._lock:
+            self._init_number += 1
+
+    def get_next_number(self):
+        return self._init_number
+
+def test_singleton_thread_safe():
+    generator = Number_Generator()
+    generator.generate_next_number()
+    print(f"Generated Number: {generator.get_next_number()}")
+
+
+
+if __name__ == '__main__':
+    # Create a list to store threads
+    threads = []
+
+    for _ in range(10):
+        t = threading.Thread(target=test_singleton_thread_safe())
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
